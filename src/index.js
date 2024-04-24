e7fefb6aadc09e4028fd035a713f71bc98a4965b
hu.po <hugo.p.cmu@gmail.com>
2024-04-24 14:39:44 -0500

    cleanup, jointvalues

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import URDFLoader from 'urdf-loader';
-// import URDFRobot from 'urdf-loader';
 
 AFRAME.registerComponent('urdf', {
   schema: {
@@ -8,73 +7,83 @@ AFRAME.registerComponent('urdf', {
   },
 
   init: function () {
-    console.log('URDF Component Initialized');
-    var el = this.el;
     var self = this;
     this.robot = null;
-    this.object3DMap = new Map();
+    this.objMap = new Map();
+    this.jointMap = new Map();
     this.urdfLoader = new URDFLoader();
-    this.urdfLoader.loadMeshCb = function (path, manager, done) {
+    this.urdfLoader.loadMeshCb = function (path, _, done) {
       console.log('Loading mesh from: ' + path);
       var name = path.split('/').pop().split('.')[0];
-      name = "link_" + name.replace(/[^a-zA-Z0-9]/g, '_');
-      self.object3DMap.set(name, path);
+      name = "link_" + name.replace(/[^a-zA-Z0-9]/g, '_'); // Sanitize name
+      self.objMap.set(name, path);
       done();
     }
+    this.el.sceneEl.addEventListener('setjoints', (jointValues) => {
+      if (!this.robot) { return; }
+      this.robot.setJointValues(jointValues.detail);
+    });
   },
 
-  update: function (oldData) {
-    var data = this.data;
-    var robot = this.robot;
-    if (!data.url) {
-      console.error('URDF URL is required');
+  update: function () {
+    if (!this.data.url) {
+      console.error('No URDF URL');
       return;
     }
-    this.resetRobot();
-    console.log('Loading URDF from: ' + data.url);
-    this.urdfLoader.load(data.url, (_robot) => {
-      robot = _robot;
-      console.log('Number of Links: ' + Object.keys(robot.links).length);
-      console.log('Number of Joints: ' + Object.keys(robot.joints).length);
-      this.traverseRobot(robot);
-      robot.setJointValues();
+    console.log('Loading URDF from: ' + this.data.url);
+    this.urdfLoader.load(this.data.url, (_robot) => {
+      this.robot = _robot;
+      console.log('Number of Links: ' + Object.keys(this.robot.links).length);
+      console.log('Number of Joints: ' + Object.keys(this.robot.joints).length);
+      this.traverseRobot(this.robot);
     });
-    // robot.setJointValues();
   },
 
   remove: function () {
-    this.resetRobot();
+    if (!this.robot) { return; }
   },
 
-  resetRobot: function () {
+  tick: function () {
     if (!this.robot) { return; }
-    // TODO
+    // // Moves random joint to random position for testing
+    const keys = Array.from(this.jointMap.keys());
+    const randomIndex = Math.floor(Math.random() * keys.length);
+    const randomJointName = keys[randomIndex];
+    const joint = this.robot.joints[randomJointName];
+    const randomPosition = joint.limit.lower + Math.random() * (joint.limit.upper - joint.limit.lower);
+    let jointValues = {};
+    jointValues[randomJointName] = randomPosition;
+    this.el.emit('setjoints', jointValues);
   },
 
   traverseRobot: function (node) {
     console.log('Visiting node: ' + node.name);
     console.log('Node type: ' + node.type);
-    if (node.type === 'URDFJoint' || node.type === 'URDFLink') {
-      // Create new entity for the link/joint
+    if (node.parent) {
+      var parentName = node.parent.name.replace(/[^a-zA-Z0-9]/g, '_');
+      var parentEntity = document.querySelector('#' + parentName);
+    }
+    if (node.type === 'URDFVisual') {
+      var path = this.objMap.get(parentName)
+      parentEntity.setAttribute('obj-model', { obj: path, mtl: this.data.mtl });
+    } else {
       var entity = document.createElement('a-entity');
       entity.setAttribute('id', node.name.replace(/[^a-zA-Z0-9]/g, '_'));
       if (node.parent) {
-        var parentName = node.parent.name.replace(/[^a-zA-Z0-9]/g, '_');
-        var parentEntity = document.querySelector('#' + parentName);
         parentEntity.appendChild(entity);
       }
       else {
         this.el.appendChild(entity);
       }
-      // TODO: This is broken!!
       entity.object3D.position.copy(node.position);
-      entity.object3D.quaternion.copy(node.quaternion)
-    } else if (node.type === 'URDFVisual') {
-      parentName = node.parent.name.replace(/[^a-zA-Z0-9]/g, '_');
-      var parentEntity = document.querySelector('#' + parentName);
-      var path = this.object3DMap.get(parentName)
-      parentEntity.setAttribute('obj-model', { obj: path, mtl: this.data.mtl });
+      entity.object3D.quaternion.copy(node.quaternion);
+      if (node.type === 'URDFJoint') {
+        if (!(node.jointType === 'fixed')) {
+          this.jointMap.set(node.name, node.jointValue[0]);
+        }
+      }
     }
+    // Continue to recurse through the children
     if (node.children && node.children.length > 0) {
       node.children.forEach(child => this.traverseRobot(child));
     }
